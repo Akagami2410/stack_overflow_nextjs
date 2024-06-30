5a49324a67e1448c1dbb6521a5e5a506a23af376
Prasanjeet <prasanjeetsinha57@gmail.com>
2024-06-30 16:18:42 +0530

    implement Theme Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,18 @@ import type { Metadata } from "next";
 
 import {ClerkProvider} from "@clerk/nextjs";
 import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
-  subsets:['latin'],
-  weight:['100','200','300','400','500','600','700','800','900'],
-  variable:'--font-inter'
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 const spaceGrotesk = Space_Grotesk({
-  subsets:['latin'],
-  weight:['300','400','500','600','700'],
-  variable:'--font-spaceGrotesk'
-})
-
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
   title: "Devflow",
@@ -26,28 +26,26 @@ export const metadata: Metadata = {
   },
 };
 
-
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      appearance={{
-        elements:{
-          formButtonPrimary:'primary-gradient',
-          footerActionLink:'primary-text-gradient hover:text-primary-500'
-        }
-      }}
-    >
-      <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`} >
-        <body>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
+      <body>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
 
