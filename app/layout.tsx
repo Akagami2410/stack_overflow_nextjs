6a317d728c295feae2489255b3b9d13999ede079
Prasanjeet <prasanjeetsinha57@gmail.com>
2024-06-30 14:53:20 +0530

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,59 @@
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import {ClerkProvider} from "@clerk/nextjs";
 import "./globals.css";
+
+const inter = Inter({
+  subsets:['latin'],
+  weight:['100','200','300','400','500','600','700','800','900'],
+  variable:'--font-inter'
+})
+const spaceGrotesk = Space_Grotesk({
+  subsets:['latin'],
+  weight:['300','400','500','600','700'],
+  variable:'--font-spaceGrotesk'
+})
+
+
+export const metadata: Metadata = {
+  title: "Devflow",
+  description:
+    "Welcome to DevFlow, where developers connect, learn, and grow together! Join our thriving community to share insights, tackle coding puzzles, and stay updated on cutting-edge technologies. Whether you're a seasoned pro or just getting started, explore solutions, engage in lively discussions, and expand your expertise with fellow developers from around the globe.",
+  icons: {
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
+
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
+    <ClerkProvider
+      appearance={{
+        elements:{
+          formButtonPrimary:'primary-gradient',
+          footerActionLink:'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
+      <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`} >
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
           {children}
         </body>
       </html>
     </ClerkProvider>
   );
 }
+
+// <SignedOut>
+// <SignInButton />
+// </SignedOut>
+// <SignedIn>
+// <UserButton />
+// </SignedIn>
