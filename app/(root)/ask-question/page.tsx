4d1b952c4858f362161354e9ccc2870be31ab725
Prasanjeet <prasanjeetsinha57@gmail.com>
2024-07-10 17:00:26 +0530

    askQuestion Page layout done and dusted

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,16 @@
-const page = () => {
-  return <div>Ask Question</div>;
+// this page is gonna be server side
+
+import Question from "@/components/forms/Question";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
